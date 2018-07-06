1d85dda7e2a6e9bff0114457890076e07d654d4a
jcgrethe <jgrethe@itba.edu.ar>
2018-07-06 13:46:35 -0300

    fix indentation

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,14 @@ Rails.application.routes.draw do
 
   api_version(module: 'api/v1', path: { value: 'api/v1' }, defaults: { format: :json }) do
     resources :users do
-        collection do
-            resources :sessions, only: [:create] do
-                collection do
-                  post :renew
-                  post :invalidate_all
-                end
-            end
+      collection do
+        resources :sessions, only: [:create] do
+          collection do
+            post :renew
+            post :invalidate_all
+          end
         end
+      end
     end
   end
-
-  
 end
