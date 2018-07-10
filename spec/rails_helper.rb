7a58fbffb296798ca989f25df2a2bfb8fb573a55
jcgrethe <jgrethe@itba.edu.ar>
2018-07-10 12:03:53 -0300

    added spec to rubocop testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
-
-
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -64,7 +62,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
-
   end
 
   config.before(:each) do
@@ -81,4 +78,4 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
-end 
+end
