072112aa03e26994c7aa539f3a04b9a22fef912c
jcgrethe <jgrethe@itba.edu.ar>
2018-07-10 16:11:36 -0300

    added suport folder to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,13 +82,3 @@ end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
-RSpec.shared_context 'Authenticated User' do
-  let(:user) { create(:user) }
-  let!(:access_data) { AuthenticableEntity.generate_access_token(user) }
-  let!(:access_token) { access_data[:token] }
-  let!(:renew_id) { access_data[:renew_id] }
-
-  before do
-    request.headers['Authorization'] = access_token
-  end
-end
