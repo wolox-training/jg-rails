5338114eb5ef714d79ca355b393d8db576b0da4d
jcgrethe <jgrethe@itba.edu.ar>
2018-07-10 13:32:39 -0300

    add not null to foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_153043) do
+ActiveRecord::Schema.define(version: 2018_07_10_162907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 2018_07_10_153043) do
   create_table "rents", force: :cascade do |t|
     t.date "from", null: false
     t.date "to", null: false
-    t.bigint "user_id"
-    t.bigint "book_id"
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
     t.index ["book_id"], name: "index_rents_on_book_id"
     t.index ["user_id"], name: "index_rents_on_user_id"
   end
