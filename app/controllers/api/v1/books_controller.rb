2ff59c139002f76e4104209beb13b56e952cb305
jcgrethe <jgrethe@itba.edu.ar>
2018-07-11 16:54:06 -0300

    fix authentication

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class BooksController < ApplicationController
+    class BooksController < ApiController
       include Wor::Paginate
       rescue_from ActiveRecord::RecordNotFound, with: :error_404
 
