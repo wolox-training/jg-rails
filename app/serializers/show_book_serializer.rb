3b5a737c78e9c85392fa8e5bca27b0971a96b851
jcgrethe <jgrethe@itba.edu.ar>
2018-07-11 12:42:43 -0300

    lazy initialization of last rent

--- app/serializers/show_book_serializer.rb
+++ app/serializers/show_book_serializer.rb
@@ -1,8 +1,7 @@
 class ShowBookSerializer < BookBaseSerializerSerializer
   attributes :image, :actual_rent
   def actual_rent
-    rents = object.rents.to_a
-    rent = rents.last
-    rents.last if Date.current < rent.to
+    last = object.rents.last
+    last if Date.current < last.to
   end
 end
