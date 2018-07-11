9dfc348ab2936055f33e692a86b67ab8bbf9affc
jcgrethe <jgrethe@itba.edu.ar>
2018-07-11 11:43:07 -0300

    move shared context to support folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -91,5 +92,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   # Kernel.srand config.seed
-  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 end
