b5f4101bf9b60f82a75393ae70e3ba2a689925c3
jcgrethe <jgrethe@itba.edu.ar>
2018-07-17 18:30:44 -0300

    added internationalization to emails

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,10 +1,18 @@
 class RentMailer < ActionMailer::Base
   default from: Rails.application.credentials.default_mail
   layout 'mailer'
+
   def new_rent(rent)
     @rent = rent
     @book = Book.find(rent.book_id)
     @user = User.find(rent.user_id)
+    locale(@user)
     mail to: @user.email
+  end
+
+  private
+
+  def locale(user)
+    I18n.locale = user.locale
   end
 end
