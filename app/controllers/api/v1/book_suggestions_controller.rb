74f6185647b42127161643b6909141da2fd608b8
jcgrethe <jgrethe@itba.edu.ar>
2018-07-19 10:30:38 -0300

    added the controller for bookSuggestion

---
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class BookSuggestionsController < ApiController
+      def create
+        book_suggestion = BookSuggestion.new(suggestion_params)
+        return render json: book_suggestion.errors, status: :unprocessable_entity unless book_suggestion.save
+        render json: book_suggestion, status: :created
+      end
+
+      def suggestion_params
+        params.require(:book_suggestion).permit(:editorial, :price, :author, :title, :link, :publisher, :year, :user_id)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :users do
       resources :rents, only: %i[create index]
     end
+    resources :book_suggestions, only: %i[create]
   end
 
   devise_for :users
