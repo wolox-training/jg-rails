0065e223e34a3d8d1322b5437ec21056bb560417
jcgrethe <jgrethe@itba.edu.ar>
2018-07-19 09:34:50 -0300

    added BookSuggestion

---
+++ db/migrate/20180718222340_create_book_suggestions.rb
@@ -0,0 +1,15 @@
+class CreateBookSuggestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :book_suggestions do |t|
+      t.string :editorial
+      t.float :price
+      t.string :author, null: false
+      t.string :title, null: false
+      t.string :link, null: false
+      t.string :publisher
+      t.string :year
+      t.references :user, foreign_key: true, null: false
+      t.timestamps
+    end
+  end
+end
