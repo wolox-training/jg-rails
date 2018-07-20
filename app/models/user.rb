e0ee009976ae7a2d7cba9a25a6545e9b85bb177f
jcgrethe <jgrethe@itba.edu.ar>
2018-07-20 17:33:48 -0300

    user authentication integrated with google api

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   validates :first_name, :last_name, presence: true
   before_validation :generate_verification_code, on: :create
   has_many :rents, dependent: :destroy
   has_many :book_suggestions, dependent: :destroy
   def generate_verification_code
     self.verification_code = AuthenticableEntity.verification_code
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email'])
+
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create(first_name: data['name'], last_name: data['last_name'], email: data['email'],
+                         password: password, password_confirmation: password)
+    end
+    user
   end
 end
